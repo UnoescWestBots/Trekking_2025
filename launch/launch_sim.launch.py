7779aa057e1214f3fa540d5f69e94cb82857c3ad
UnoescWestBots <unoesc.westbots@gmail.com>
2025-03-26 18:34:43 -0300

    Arquivos Trekking Iniciais

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -2,14 +2,11 @@ import os
 
 from ament_index_python.packages import get_package_share_directory
 
-
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
-
-
 
 def generate_launch_description():
 
@@ -21,19 +18,27 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true'}.items()
     )
 
+    # Use get_package_share_directory to get the correct path for the world file
+    world_file = os.path.join(
+                    get_package_share_directory('traxxas'), 'worlds', 'empty.world'
+    )  
+
     # Include the Gazebo launch file, provided by the gazebo_ros package
     gazebo = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+        PythonLaunchDescriptionSource([
+            os.path.join(
+                get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py'
+            )]),
+        launch_arguments={'world': world_file}.items()
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                        arguments=['-topic', 'robot_description',
-                                   '-entity', 'traxxas'],
-                        output='screen')
-
-
+    spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', '/robot_description', '-entity', 'traxxas'],
+        output='screen'
+    )
 
     # Launch them all!
     return LaunchDescription([
