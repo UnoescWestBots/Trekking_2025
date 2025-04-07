a657da41a4124baff7592dd504abe2ea49c578eb
UnoescWestBots <unoesc.westbots@gmail.com>
2025-04-07 16:43:40 -0300

    Atualização dos Arquivos para o Trekking

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = "Trekking_2025"
+
+setup(
+    name=package_name,
+    version="0.0.0",
+    packages=find_packages(exclude=["test"]),
+    data_files=[
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        (os.path.join("share", package_name, "urdf"), glob("urdf/*")),
+        (os.path.join("share", package_name, "meshes"), glob("meshes/*")),
+        (os.path.join("share", package_name, "worlds"), glob("worlds/*")),
+        (os.path.join("share", package_name, "launch"), glob("launch/*")),
+        (os.path.join("share", package_name, "rviz"), glob("rviz/*")),
+    ],
+    install_requires=["setuptools"],
+    zip_safe=True,
+    maintainer="nuke",
+    maintainer_email="unoesc.westbotsr@gmail.com",
+    description="TODO: Package description",
+    license="TODO: License declaration",
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": [],
+    },
+)
